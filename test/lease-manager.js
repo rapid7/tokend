a7c2ee62c1010cfe0abce057727162a2d0c2f63f
Frank Mitchell <me@frankmitchell.org>
2016-04-04 15:55:28 -0400

    Change status and data when the provider's initialized.

--- test/lease-manager.js
+++ test/lease-manager.js
@@ -15,6 +15,15 @@ class DoNothingProvider {
   }
 }
 
+/**
+ * A mock SecretProvider that succeeds on initialize
+ */
+class InitializeSucceedProvider {
+  initialize(callback) {
+    callback(null, 'SECRET');
+  }
+}
+
 describe('LeaseManager#constructor', function () {
   it('has a default status of PENDING', function () {
     should(new LeaseManager(new DoNothingProvider()).status).eql('PENDING');
@@ -22,5 +31,13 @@ describe('LeaseManager#constructor', function () {
 
   it('has default data that is null', function () {
     should(new LeaseManager(new DoNothingProvider()).data).be.null();
+  });
+
+  it('changes status to READY when the provider succeeds', function () {
+    should(new LeaseManager(new InitializeSucceedProvider()).status).eql('READY');
+  });
+
+  it('changes data to a secret when the provider succeeds', function () {
+    should(new LeaseManager(new InitializeSucceedProvider()).data).eql('SECRET');
   });
 });
