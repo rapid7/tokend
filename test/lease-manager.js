b2768db2141b176f416dfc80838a22c544e39958
Frank Mitchell <me@frankmitchell.org>
2016-04-04 15:05:19 -0400

    Allow LeaseManagers to be created with SecretProviders.

--- test/lease-manager.js
+++ test/lease-manager.js
@@ -4,12 +4,23 @@ const should = require('should');
 
 const LeaseManager = require('../lib/lease-manager');
 
+/**
+ * A mock SecretProvider that does nothing on initialize or renew
+ */
+class DoNothingProvider {
+  initialize() {
+  }
+
+  renew() {
+  }
+}
+
 describe('LeaseManager#constructor', function () {
   it('has a default status of PENDING', function () {
-    should(new LeaseManager().status).eql('PENDING');
+    should(new LeaseManager(new DoNothingProvider()).status).eql('PENDING');
   });
 
   it('has default data that is null', function () {
-    should(new LeaseManager().data).be.null();
+    should(new LeaseManager(new DoNothingProvider()).data).be.null();
   });
 });
