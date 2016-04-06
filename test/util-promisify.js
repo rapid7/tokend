8815ca959bd32fa37031c0cc5749f8e3e380ee7e
davepgreene <davepgreene@gmail.com>
2016-04-06 17:42:16 -0400

    Updated promisify tests so there are no issue with test function scopes (see https://mochajs.org/#arrow-functions)

--- test/util-promisify.js
+++ test/util-promisify.js
@@ -1,4 +1,3 @@
-/* eslint-env mocha */
 'use strict';
 
 const chai = require('chai');
@@ -18,16 +17,16 @@ const pass = (callback) => {
   });
 };
 
-describe('Util/Promisify', () => {
-  it('should wrap the method in a promise', () => {
+describe('Util/Promisify', function() {
+  it('should wrap the method in a promise', function() {
     promisify((c) => pass(c)).should.be.a.Promise();
   });
 
-  it('should reject with an error if the promise-ified method returns one', () => {
+  it('should reject with an error if the promise-ified method returns one', function() {
     return promisify((c) => fail(c)).should.be.rejectedWith(Error, {message: 'Some error message'});
   });
 
-  it('should pass and return data if the promise-ified method returns a result', () => {
+  it('should pass and return data if the promise-ified method returns a result', function() {
     return promisify((c) => pass(c)).should.eventually.eql({
       foo: 'bar',
       baz: true
