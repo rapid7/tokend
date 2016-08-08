cf9ab4dc38612015f23d99c5f9b204a8382915a2
davepgreene <davepgreene@gmail.com>
2016-08-08 10:21:57 -0400

    Add a cookbook for installing and configuring tokend

---
+++ cookbook/attributes/default.rb
@@ -0,0 +1,9 @@
+default['tokend']['user'] = 'tokend'
+default['tokend']['group'] = 'tokend'
+
+default['tokend']['paths']['directory'] = '/opt/tokend'
+default['tokend']['paths']['executable'] = ::File.join(node['tokend']['paths']['directory'], 'bin/server.js')
+default['tokend']['paths']['configuration'] = '/etc/tokend/config.json'
+
+default['tokend']['config'] = Mash.new
+default['tokend']['version'] = nil
---
+++ cookbook/libraries/helpers.rb
@@ -0,0 +1,21 @@
+class Chef::Recipe
+  ##
+  # Allow a recipe to find its cookbook's version
+  ##
+  def cookbook_version
+    run_context.cookbook_collection[cookbook_name].version
+  end
+end
+
+##
+# Version and Download URI helpers
+##
+module Tokend
+  module Helpers
+    class << self
+      def github_download(owner, repo, version)
+        "https://github.com/#{owner}/#{repo}/releases/download/v#{version}/tokend_#{version}_amd64.deb"
+      end
+    end
+  end
+end
