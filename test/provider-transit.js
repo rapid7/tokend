b372bc7c98c6968f837cc51344ab30dd9688d537
Frank Mitchell <me@frankmitchell.org>
2016-09-29 11:51:53 -0400

    Ensure token is provided as an argument to the constructor.

--- test/provider-transit.js
+++ test/provider-transit.js
@@ -13,5 +13,11 @@ describe('Provider/Transit', function () {
         return new TransitProvider('', 'TOKEN', {ciphertext: 'CTEXT'});
       }, preconditions.IllegalValueError);
     });
+
+    it('throws an IllegalValueError if the token is not provided', function () {
+      should.throws(() => {
+        return new TransitProvider('KEY', '', {ciphertext: 'CTEXT'});
+      }, preconditions.IllegalValueError);
+    });
   });
 });
