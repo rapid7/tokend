b97af869d70911fcb2c253df4fce01c0b57d0044
Frank Mitchell <me@frankmitchell.org>
2016-09-30 08:20:38 -0400

    Use global mock Vault server for transit tests.

--- test/provider-transit.js
+++ test/provider-transit.js
@@ -8,7 +8,53 @@ const preconditions = require('conditional');
 const nock = require('nock');
 const should = require('should');
 
+/**
+ * Create a mock Vault server
+ *
+ * @param {Object} options - Options to use when setting up a mock Vault server
+ * @param {Boolean} options.global - True if the mocked Vault server should define setup API calls
+ * @return {Nock} - The mock Vault server
+ */
+function createVaultMock(options) {
+  const vaultHostname = `${(Config.get('vault:tls')) ? 'https' : 'http'}://${Config.get('vault:host')}:${Config.get('vault:port')}/`;
+
+  if (!options.global) {
+    return nock(vaultHostname);
+  }
+
+  return nock(vaultHostname).persist()
+    .get('/v1/sys/init')
+    .reply(STATUS_CODES.OK, {initialized: true})
+    .get('/v1/sys/seal-status')
+    .reply(STATUS_CODES.OK, {sealed: false, t: 1, n: 1, progress: 1})
+    .get('/v1/sys/mounts')
+    .reply(STATUS_CODES.OK, {'transit/': {config: {default_lease_ttl: 0, max_lease_ttl: 0}, type: 'transit'}})
+    .get('/v1/sys/auth')
+    .reply(STATUS_CODES.OK, {'token/': {type: 'token'}})
+    .post('/v1/sys/mounts/transit', {type: 'transit', description: 'Transit Secrets Backend transit'})
+    .reply(STATUS_CODES.OK, {'token/': {type: 'token'}});
+}
+
 describe('Provider/Transit', function () {
+  let globalVaultMock = null,
+      localVaultMock = null;
+
+  before(function () {
+    globalVaultMock = createVaultMock({global: true});
+  });
+
+  beforeEach(function () {
+    localVaultMock = createVaultMock({global: false});
+  });
+
+  afterEach(function () {
+    nock.removeInterceptor(localVaultMock);
+  });
+
+  after(function () {
+    nock.removeInterceptor(globalVaultMock);
+  });
+
   describe('TransitProvider#constructor', function () {
     it('requires key be provided', function () {
       [null, undefined, ''].forEach(function (value) {
@@ -43,46 +89,7 @@ describe('Provider/Transit', function () {
     });
   });
 
-  function createVaultMock(options) {
-    const vaultHostname = `${(Config.get('vault:tls')) ? 'https' : 'http'}://${Config.get('vault:host')}:${Config.get('vault:port')}/`;
-
-    if (!options.global) {
-      return nock(vaultHostname);
-    }
-
-    return nock(vaultHostname).persist()
-      .get('/v1/sys/init')
-      .reply(STATUS_CODES.OK, {initialized: true})
-      .get('/v1/sys/seal-status')
-      .reply(STATUS_CODES.OK, {sealed: false, t: 1, n: 1, progress: 1})
-      .get('/v1/sys/mounts')
-      .reply(STATUS_CODES.OK, {'transit/': {config: {default_lease_ttl: 0, max_lease_ttl: 0}, type: 'transit'}})
-      .get('/v1/sys/auth')
-      .reply(STATUS_CODES.OK, {'token/': {type: 'token'}})
-      .post('/v1/sys/mounts/transit', {type: 'transit', description: 'Transit Secrets Backend transit'})
-      .reply(STATUS_CODES.OK, {'token/': {type: 'token'}});
-  }
-
   describe('TransitProvider#initialize', function () {
-    let globalVaultMock = null,
-        localVaultMock = null;
-
-    before(function () {
-      globalVaultMock = createVaultMock({global: true});
-    });
-
-    beforeEach(function () {
-      localVaultMock = createVaultMock({global: false});
-    });
-
-    afterEach(function () {
-      nock.removeInterceptor(localVaultMock);
-    });
-
-    after(function () {
-      nock.removeInterceptor(globalVaultMock);
-    });
-
     it('calls Vault once when initializing', function (done) {
       const transit = new TransitProvider('KEY', 'TOKEN', {ciphertext: 'CTEXT'});
 
