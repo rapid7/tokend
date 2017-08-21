1cd593b9077a6be45ce01d4ea639580b915f40cb
davepgreene <davepgreene@gmail.com>
2017-08-21 16:19:38 -0400

    Updated warden helper to use node-vault and also to send the same data that the real warden application does.

--- test/bin/warden.js
+++ test/bin/warden.js
@@ -2,71 +2,97 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const Vaulted = require('vaulted');
+const Vault = require('node-vault');
 
-const DEFAULT_PORT = 3000;
+const DEFAULT_PORT = 8705;
 const VAULT_PORT = 8200;
 const token = process.argv[2];
 
 const app = express();
 const router = express.Router();
-const vault = new Vaulted({
-  vault_host: '127.0.0.1',
-  vault_port: VAULT_PORT,
-  vault_ssl: false
+const vault = new Vault({
+  endpoint: `http://127.0.0.1:${VAULT_PORT}`,
+  token
 });
 
 app.use(bodyParser.json());
 app.use(router);
-
-vault.setToken(token).prepare();
 
 router.param('id', (req, res, next, id) => {
   req.id = id;
   next();
 });
 
-router.route('/').post((req, res) => {
-  vault.createToken({
-    body: {
-      ttl: '1m'
+router.route('/v1/authenticate').post((req, res) => {
+  vault.mounts().then((mounts) => {
+    if (!mounts.hasOwnProperty('transit/')) {
+      vault.mount({
+        mount_point: 'transit',
+        type: 'transit'
+      });
     }
-  })
-  .then((data) => {
-    const resp = {
-      lease_duration: data.auth.lease_duration,
-      renewable: data.auth.renewable,
-      data: {
-        token: data.auth.client_token
-      }
-    };
+  }).then(() => {
+    let token;
 
-    console.log(resp);
-    res.json(resp);
-  })
-  .catch((err) => {
+    vault.tokenCreate({
+      ttl: '1m',
+      renewable: true,
+      no_parent: true
+    }).then((data) => {
+      token = data.auth;
+
+      return vault.tokenLookupAccessor({accessor: data.auth.accessor});
+    }).then((resp) => {
+      const creation_time = resp.data.creation_time * 1000;
+      const explicit_max_ttl = resp.data.explicit_max_ttl * 1000;
+
+      if (explicit_max_ttl === 0) {
+        return vault.request({
+          path: '/sys/mounts/auth/token/tune',
+          method: 'GET'
+        }).then((resp) => ({
+          creation_time,
+          explicit_max_ttl: resp.max_lease_ttl * 1000
+        }));
+      }
+
+      return {
+        creation_time,
+        explicit_max_ttl
+      };
+    }).then((data) => ({
+      creation_time: new Date(data.creation_time).toISOString(),
+      expiration_time: new Date(data.creation_time + data.explicit_max_ttl).toISOString()
+    })).then((data) => {
+      if (data.expiration_time <= data.creation_time) {
+        throw new Error('Token has already expired');
+      }
+
+      const resp = Object.assign({}, token, data);
+
+      console.log(resp);
+      res.json(resp);
+    });
+  }).catch((err) => {
     console.log(err);
-    res.status(err.statusCode)
-        .json(err.error);
+    res.status(err.statusCode).json(err.error);
   });
 });
 
 router.route('/mounts').get((req, res) => {
-  vault.getMounts()
+  vault.mounts()
     .then((mounts) => res.json(mounts))
     .catch((err) => res.status(err.statusCode).json(err.error));
 });
 
 router.route('/auths').get((req, res) => {
-  vault.getAuthMounts()
+  vault.auths()
     .then((authMounts) => res.json(authMounts))
     .catch((err) => res.status(err.statusCode).json(err.error));
 });
 
 router.route('/secret/:id').get((req, res) => {
-  vault.read({
-    id: req.id
-  }).then((secret) => {
+  vault.read(req.id).then((secret) => {
     res.status(res.statusCode).json(secret);
   });
 });
